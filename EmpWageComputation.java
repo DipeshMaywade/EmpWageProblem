6ac3d560523f298d9463f417b9ef1a6ad9fad349
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-10 15:31:27 +0530

    [Deep]Update: Merge file with UC1 branch

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,8 +1,13 @@
-
 public class EmpWageComputation {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
+	public static void main(String[] args) {
+		int isFullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 
-    }
+		if (empCheck == 1)
+			System.out.println("Employee Is Present");
+		else
+			System.out.println("Employye Is Abscent");
+	}
+
 }
