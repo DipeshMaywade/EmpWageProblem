42a7fd375cb2b09dbe470cf983a3b1ea61f38857
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-10 15:47:46 +0530

    [Deep]Updated: file updated for DailyWage Calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,12 +2,21 @@ public class EmpWageComputation {
 
 	public static void main(String[] args) {
 		int isFullTime = 1;
+		int wagePerHr = 20;
+		int fullDayHr = 8;
+		int dailyWage;
+
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 
-		if (empCheck == 1)
+		if (empCheck == 1){
 			System.out.println("Employee Is Present");
-		else
+			dailyWage = wagePerHr*fullDayHr;
+		}
+		else{
 			System.out.println("Employye Is Abscent");
-	}
+			dailyWage = 0;
+		}
 
+		System.out.println("Employee Daily Salary is: " + dailyWage);
+	}
 }
