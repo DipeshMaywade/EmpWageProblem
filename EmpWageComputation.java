a40e495c4c2dce6f35ce84efdf7d97471505c95c
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-15 00:14:45 +0530

    [Deep]Updated: file updated for Class variables(OOP)

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,32 +1,42 @@
 public class EmpWageComputation {
 
+	private static final int IS_FULL_TIME = 1;
+	private static final int IS_PART_TIME = 2;
+
+	private static int wagePerHr;
+	private static int maxMonthHr;
+	private static int workingDays;
+	private static int fullTimeHr;
+	private static int partTimeHr;
+
+	public EmpWageComputation(){
+		wagePerHr = 20;
+		maxMonthHr = 100;
+		workingDays = 20;
+		fullTimeHr = 8;
+		partTimeHr = 4;
+	}
+
 	public static void main(String[] args) {
-		int isFullTime = 1;
-		int isPartTime = 2;
-		int wagePerHr = 20;
-		int maxHr = 8;
-		int maxMonthHr = 100;
-		int empHr = 0;
-		int partTimeHr = 4;
-		int workingDays = 20;
-		int totalEmpHr = 0;
-
-		int hr = 0;
+		int empHr=0;
+		int totalEmpHr=0;
 		int days=0;
-		while (empHr<=maxMonthHr && days<=workingDays) {
 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		int empCheck1 = (int)empCheck;
+		new EmpWageComputation();
+		while (empHr<= maxMonthHr && days<= workingDays) {
 
-		switch(empCheck1){
-			case 1:
-	                        empHr = maxHr;
-				break;
-			case 2:
-                 		empHr = partTimeHr;
-				break;
-			default:
-				empHr = empHr;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			int empCheck1 = (int)empCheck;
+
+			switch(empCheck1){
+				case IS_FULL_TIME:
+					empHr = fullTimeHr;
+					break;
+				case IS_PART_TIME:
+					empHr = partTimeHr;
+					break;
+				default:
+					empHr = 0;
 			}
 
 			totalEmpHr=totalEmpHr+empHr;
